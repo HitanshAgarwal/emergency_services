d7a799ec416c96b5d58f8d2f5773962603a3544e
amani-agrawal <50449831+amani-agrawal@users.noreply.github.com>
2022-07-29 23:39:14 +0530

    Delete main.dart

--- main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:emergency_services/askRorLCustomer.dart';
-import 'package:emergency_services/askRorLHospital.dart';
-import 'package:emergency_services/askStatus.dart';
-
-
-void main() {
-  runApp(MaterialApp(home:askStatus()));
-}
